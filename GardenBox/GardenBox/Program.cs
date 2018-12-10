399ea323b9d569710edeb7496602131f9d0f31bb
Michael Huffman <huffman442@yahoo.com>
2018-12-10 14:05:42 -0500

    Added Garden Box Project

---
+++ GardenBox/GardenBox/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GardenBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Take in Width & Length
+            Console.WriteLine("What is the width of your garden box?");
+            double width = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("What is the length of your garden box?");
+            double length = Convert.ToDouble(Console.ReadLine());
+
+            //Calculate Area
+
+            //Veggies Per Sq. Ft.
+
+            //Display Veggies Per Box
+
+            Console.ReadLine();
+        }
+    }
+}
