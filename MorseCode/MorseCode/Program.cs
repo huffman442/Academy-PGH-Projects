02d9046abf8fba038c3e9e28d0aeba2ff5526f24
Michael Huffman <huffman442@yahoo.com>
2018-12-10 21:58:54 -0500

    Created morse code library, & added map for conversion

--- MorseCode/MorseCode/Program.cs
+++ MorseCode/MorseCode/Program.cs
@@ -10,6 +10,40 @@ namespace MorseCode
     {
         static void Main(string[] args)
         {
+            //Create morse code library
+            string[] morseLibrary = new string[27];
+            morseLibrary[0] = ".-";
+            morseLibrary[1] = "-...";
+            morseLibrary[2] = "-.-.";
+            morseLibrary[3] = "-..";
+            morseLibrary[4] = ".";
+            morseLibrary[5] = "..-.";
+            morseLibrary[6] = "--.";
+            morseLibrary[7] = "....";
+            morseLibrary[8] = "..";
+            morseLibrary[9] = ".---";
+            morseLibrary[10] = "-.-";
+            morseLibrary[11] = ".-..";
+            morseLibrary[12] = "--";
+            morseLibrary[13] = "-.";
+            morseLibrary[14] = "---";
+            morseLibrary[15] = ".--.";
+            morseLibrary[16] = "--.-";
+            morseLibrary[17] = ".-.";
+            morseLibrary[18] = "...";
+            morseLibrary[19] = "-";
+            morseLibrary[20] = "..-";
+            morseLibrary[21] = "...-";
+            morseLibrary[22] = ".--";
+            morseLibrary[23] = "-..-";
+            morseLibrary[24] = "-.--";
+            morseLibrary[25] = "--..";
+            morseLibrary[26] = " ";
+
+            //Map for conversion
+            string map = "abcdefghijklmnopqrstuvwxyz ";
+
+
         }
     }
 }
